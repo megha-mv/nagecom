b93e72beb64d5ad8b2e06700c008eea27bea3aee
Megha <megha@nagarro.com>
2024-08-07 18:52:30 +0530

    react ecommerce basic app

--- nagecom/src/components/navbar.jsx
+++ nagecom/src/components/navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="links">
-        <Link to="/"> Shop </Link>
+        <Link to="/"> HOme </Link>
         <Link to="/contact"> Contact </Link>
         <Link to="/cart">
           <GiShoppingCart size={32} />
